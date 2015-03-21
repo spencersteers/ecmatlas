4fb542fb04852ad9350ccc5dad9a0f0a3eadea2f
ssteers@levelsbeyond.com <Spencer Steers>
2015-03-21 15:27:19 -0600

    update models for new dataset

--- experiments/models.py
+++ experiments/models.py
@@ -2,8 +2,23 @@ from django.db import models
 from atlas.models import Tissue, Family, FunctionalGroup, Protein
 
 
-# class Experiment(models.Model):
-#     pass
+class Experiment(models.Model):
+
+    name = models.CharField(max_length=255, null=True)
+    aquisition_date = models.DateField(null=True)
+    species = models.CharField(max_length=255, null=True)
+    data_type = models.CharField(max_length=255, null=True)
+    aquisition_instrument = models.CharField(max_length=255, null=True)
+    aquisition_type = models.CharField(max_length=255, null=True)
+    publication = models.URLField(null=True)
+    data_depository_link = models.CharField(max_length=255, null=True)
+    slug = AutoSlugField(populate_from='get_full_name')
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_full_name():
+        return str(self.name) + str(self.aquisition_date)
 
 
 class Dataset(models.Model):
@@ -12,6 +27,7 @@ class Dataset(models.Model):
     data_file = models.FileField(upload_to='uploads/datasets')
     is_inserted = models.BooleanField(default=False)
     inserted_at = models.DateTimeField(null=True)
+    experiment = models.ForeignKey(Experiment)
 
     def __str__(self):
         return str(self.name) + " (" + str(self.inserted_at) + ")"
@@ -33,3 +49,16 @@ class DatasetItem(models.Model):
 
     def __str__(self):
         return str(self.dataset) + ": " + str(self.protein) + " " + str(self.tissue)
+
+
+class RelativeConcentration(models.Model):
+
+    sex = models.CharField(max_length=255, null=True)
+    disease_state = models.CharField(max_length=255, null=True)
+    disease_note = models.CharField(max_length=255, null=True)
+    age = models.IntegerField(null=True)
+    value = models.FloatField()
+    protein = models.ForeignKey(Protein)
+    tissue = models.ForeignKey(Tissue)
+    dataset = models.ForeignKey(Dataset)
+
