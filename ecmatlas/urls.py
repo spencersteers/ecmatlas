51133c1911266b954e6f4b994e7485cb82b30300
ssteers@levelsbeyond.com <Spencer Steers>
2015-05-14 03:16:12 -0600

    im stupid

---
+++ ecmatlas/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ecmatlas.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('atlas.urls')),
+    url(r'^', include('experiments.urls')),
+    url(r'^', include('dataloader.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
+)
